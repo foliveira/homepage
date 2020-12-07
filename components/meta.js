c8c693aae18564e607903f7a27c55e6ccae475ad
Fábio Oliveira <fabio.an.oliveira@gmail.com>
2020-12-07 21:47:02 +0000

    Update meta description

--- components/meta.js
+++ components/meta.js
@@ -33,7 +33,7 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content="Fábio Oliveira"
+        content="Fábio Oliveira's own corner in the internet"
       />
     </Head>
   )
