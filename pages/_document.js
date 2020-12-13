d2ec536c370a56330db33d8ef9dbfcdbdb616fcd
Fábio Oliveira <fabio.an.oliveira@gmail.com>
2020-12-13 01:23:16 +0000

    Adds Cloudflare Analytics snippet

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,12 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          
+          <!-- Cloudflare Web Analytics -->
+          <script defer src='https://static.cloudflareinsights.com/beacon.min.js' 
+                  data-cf-beacon='{"token": "f46cc27c2e6c4affba164d05a41199b7"}'>
+          </script>
+          <!-- End Cloudflare Web Analytics -->
         </body>
       </Html>
     )
