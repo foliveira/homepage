595e767d72f7d206ec91a15ac85afc5c296db4de
Fábio Oliveira <foliveira@users.noreply.github.com>
2023-01-02 00:13:35 +0000

    Update index.js
    Add Mastodon profile; Remove GitLab

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 import Title from '../components/title'
 import Social from '../components/social'
 import Container from '../components/container'
-import { FaGithub, FaTwitter, FaLinkedin, FaGitlab, FaPen } from 'react-icons/fa';
+import { FaGithub, FaTwitter, FaLinkedin, FaPen, FaMastodon } from 'react-icons/fa';
 
 export default function Index() {
   return (
@@ -22,11 +22,11 @@ export default function Index() {
             <Social url="https://twitter.com/fanoliveira">
               <FaTwitter size="32px"/>
             </Social>
+            <Social url="https://mastodon.social/@foliveira">
+              <FaMastodon size="32px"/>
+            </Social>
             <Social url="https://github.com/foliveira">
                 <FaGithub size="32px"/>
-            </Social>
-            <Social url="https://gitlab.com/fanoliveira">
-                <FaGitlab size="32px"/>
             </Social>
             <Social url="https://www.linkedin.com/in/fanoliveira/">
                 <FaLinkedin size="32px"/>
