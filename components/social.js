55f02f19549f881366de5bd4a1fae0568d16f6b1
Fábio Oliveira <foliveira@users.noreply.github.com>
2023-07-05 23:40:48 +0100

    Add social following for Mastodon  (#30)
    * Update social.js
    * Update index.js
    
    Update Mastodon target

--- components/social.js
+++ components/social.js
@@ -1,6 +1,6 @@
-export default function Social({ url, children }) {
+export default function Social({ url, children, rel="noopener"}) {
   return (
-    <a href={url} target="_blank" rel="noopener" className="mx-3 font-bold py-3 px-4 mb-6 lg:mb-0">
+    <a href={url} target="_blank" rel={rel} className="mx-3 font-bold py-3 px-4 mb-6 lg:mb-0">
       { children }
     </a>
   )
